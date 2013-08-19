7b13c7958399fed959d0f9a882df11aad43d9966
Conrad Lee <conradlee@gmail.com>
2013-08-19 19:56:54 +0100

    Added edgelist loader, license, requirements

---
+++ settings.py
@@ -0,0 +1,14 @@
+N_FOLDS = 10
+MIN_ATTR_INSTANCES = 4
+CACHED_TABLES_FOLDER = '/home/conradlee/facebook100-infer/cached/'
+SRC_DATA_DIR = '/home/conradlee/Downloads/facebook100/'
+HOLDOUT_DIR = '/home/conradlee/facebook100-infer/holdouts/'
+
+OTHER_CATEGORY_THRESHOLD = 10
+ATTRS_DTYPE = [('student_fac', 'u1'),
+               ('gender', 'u1'),
+               ('first_major', 'u4'),
+               ('second_major', 'u4'),
+               ('dorm', 'u4'),
+               ('year', 'i4'),
+               ('high_school', 'u4')]
